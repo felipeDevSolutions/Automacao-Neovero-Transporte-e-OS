2e2ac78a7dcddce95ed85f447c66de041dfff276
Felipe R. Costa <137924496+felipeDevSolutions@users.noreply.github.com>
2024-10-20 17:58:56 -0300

    Update exportarTabelaXLSX.js

--- exportarTabelaXLSX.js
+++ exportarTabelaXLSX.js
@@ -1,6 +1,23 @@
 function exportarTabelaXLSX() {
-    var nomeArquivo = "Smart Flow - ";
+    var nomeArquivo = "Smart Inventário - ";
     var tabela = document.getElementById("tabelaEquipamentos");
+
+    // Obter as células da tabela
+    var cells = tabela.querySelectorAll("td");
+
+    // Percorrer as células e converter as colunas "N Série" e "Patrimônio" para texto
+    cells.forEach(function(cell) {
+        if (cell.textContent.trim() === 'N Série' || cell.textContent.trim() === 'Patrimônio') {
+            // Obter as células de dados da mesma coluna
+            var columnCells = tabela.querySelectorAll(`td:nth-child(${cell.cellIndex + 1})`);
+
+            // Converter os valores para texto para manter os zeros à esquerda
+            columnCells.forEach(function(dataCell) {
+                dataCell.textContent = dataCell.textContent.toString();
+            });
+        }
+    });
+
     var wb = XLSX.utils.table_to_book(tabela, {sheet: "Equipamentos"});
     XLSX.writeFile(wb, nomeArquivo + ".xlsx", {sheet: nomeArquivo});
 }
